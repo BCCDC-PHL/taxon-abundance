453b69d05b966d682dfe91ab1a7b70107ab87f0f
Dan Fornika <dfornika@gmail.com>
2021-07-28 22:06:07 -0700

    Add fields abundance_(num)_ncbi_taxonomy_id and abundance_(num)_num_assigned_reads

--- bin/bracken_top_n_linelist.py
+++ bin/bracken_top_n_linelist.py
@@ -20,7 +20,7 @@ def parse_bracken_report(bracken_report_path):
 def main(args):
     bracken_report = parse_bracken_report(args.bracken_report)
 
-    bracken_report_sorted = sorted(bracken_report, key=lambda k: k['fraction_total_reads'], reverse=True) 
+    bracken_report_sorted = sorted(bracken_report, key=lambda k: k['fraction_total_reads'], reverse=True)
     
     output_fields = ['sample_id', 'taxonomy_level']
     output_line = {
@@ -30,12 +30,28 @@ def main(args):
     
     for n in range(args.top_n):
         num = str(n + 1)
+
         name_field = 'abundance_' + num + '_name'
         try:
             output_line[name_field] = bracken_report_sorted[n]['name']
         except IndexError as e:
             output_line[name_field] = "None"
         output_fields.append(name_field)
+
+        taxonomy_id_field = 'abundance_' + num + '_ncbi_taxonomy_id'
+        try:
+            output_line[taxonomy_id_field] = bracken_report_sorted[n]['taxonomy_id']
+        except IndexError as e:
+            output_line[taxonomy_id_field] = "None"
+        output_fields.append(taxonomy_id_field)
+
+        num_assigned_reads_field = 'abundance_' + num + '_num_assigned_reads'
+        try:
+            output_line[num_assigned_reads_field] = bracken_report_sorted[n]['new_est_reads']
+        except IndexError as e:
+            output_line[num_assigned_reads_field] = 0
+        output_fields.append(num_assigned_reads_field)
+
         fraction_total_reads_field = 'abundance_' + num + '_fraction_total_reads'
         try:
             output_line[fraction_total_reads_field] = bracken_report_sorted[n]['fraction_total_reads']
